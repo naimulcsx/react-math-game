10a23aacd1d4171533b643c62aa06839a985d920
Naimul Haque <im.tumul@gmail.com>
2018-06-10 21:56:45 +0600

    Added CSS media queries

--- src/components/MultiplicationGame.js
+++ src/components/MultiplicationGame.js
@@ -164,7 +164,7 @@ export default class MultiplicationGame extends React.Component {
             this.setState(() => ({
                 message: null
             }));
-        }, 1000);
+        }, 500);
 
         this.setState(() => ({
             timeOut: timeOut
